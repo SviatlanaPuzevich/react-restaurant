41274b215e74841a74b426db78cec431d9bcacb7
wheercool <a.rudy@sam-solutions.net>
2024-10-04 12:17:35 +0300

    fixed shortcomings

--- food-order/src/components/reviews/Review.jsx
+++ food-order/src/components/reviews/Review.jsx
@@ -3,7 +3,7 @@ export function Review({ review }) {
   return (
     <div className={styles.reviewCard}>
       <div key={review.id}>{review.text}</div>
-      <div className="review--rating">{review.rating}</div>
+      <div>{review.rating}</div>
     </div>
   );
 }
