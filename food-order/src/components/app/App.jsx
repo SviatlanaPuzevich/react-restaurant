8a62a1f93d3f20b9648d0c0fea35f8c2d81cf9c5
wheercool <a.rudy@sam-solutions.net>
2024-10-18 19:57:15 +0300

    move scroll and cart

--- food-order/src/components/app/App.jsx
+++ food-order/src/components/app/App.jsx
@@ -1,11 +1,9 @@
 import { Layout } from "../layout/Layout";
-import { ScrollProgress } from "../scrollProgress/ScrollProgress";
 import { ThemeContextProvider } from "../themeContex/ThemeContext";
 import { AuthContextProvider } from "../authContext/AuthContext";
 import { Provider } from "react-redux";
 import { store } from "../../redux/store";
 import { RestaurantView } from "../restaurantView/RestaurantView";
-import { Cart } from "../cart/Cart";
 
 export function App() {
   return (
@@ -13,9 +11,7 @@ export function App() {
       <ThemeContextProvider>
         <AuthContextProvider>
           <Layout>
-            <ScrollProgress />
             <RestaurantView />
-            <Cart />
           </Layout>
         </AuthContextProvider>
       </ThemeContextProvider>
