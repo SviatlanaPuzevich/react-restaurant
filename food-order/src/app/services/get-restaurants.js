8f6e668bd8e3763567871281e9f9c5fda9914161
wheercool <a.rudy@sam-solutions.net>
2024-11-01 22:38:48 +0200

    revalidation

--- food-order/src/app/services/get-restaurants.js
+++ food-order/src/app/services/get-restaurants.js
@@ -1,4 +1,6 @@
 export async function getRestaurants() {
-  const result = await fetch("http://localhost:3001/api/restaurants");
+  const result = await fetch("http://localhost:3001/api/restaurants", {
+    next: { tags: ["restaurants"] },
+  });
   return result.json();
 }
